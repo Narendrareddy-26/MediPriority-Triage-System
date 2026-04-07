a366e5845115041d46c5a4c4c1a2843e40ca65bf
NithyanandhReddy16 <p.nithyanandh44@gmail.com>
2026-04-07 15:33:28 +0530

    Delete Downloads/files (1) directory

--- Downloads/files (1)/university-backend/university-backend/src/main/java/com/university/portal/repository/AssignmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.university.portal.repository;
-
-import com.university.portal.entity.Assignment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
-}
